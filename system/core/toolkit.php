42ca6ca0c43ef518dd4911e0e16bb313803823d5
Manuele J Sarfatti <mjsarfatti@gmail.com>
2011-05-12 16:29:34 +0200

    fixed time constants definition

--- system/core/toolkit.php
+++ system/core/toolkit.php
@@ -24,18 +24,19 @@
  * ------------------------------------------------------------------------
  */
 
+
 /*
  |-------------------------------------------------------------------------
  |  Constants
  |-------------------------------------------------------------------------
 */
 
-define(SECOND, 1);
-define(MINUTE, 60 * SECOND);
-define(HOUR, 60 * MINUTE);
-define(DAY, 24 * HOUR);
-define(MONTH, 30 * DAY);
-define(YEAR, 365 * DAY);
+define('SECOND', 1);
+define('MINUTE', 60 * SECOND);
+define('HOUR', 60 * MINUTE);
+define('DAY', 24 * HOUR);
+define('MONTH', 30 * DAY);
+define('YEAR', 365 * DAY);
 
 
 /*
